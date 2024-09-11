1bbd03371bae890397aab3a7ce2532c022f53247
souta9351 <56113417+snow9351@users.noreply.github.com>
2024-09-12 00:59:38 +0530

    Update Location.jsx

--- src/sidebar/Location.jsx
+++ src/sidebar/Location.jsx
@@ -3,16 +3,15 @@ import InputField from '../components/InputField'
 
 
 const Location = ({handleChange,handleClick}) => {
+  const [location, setLocation] = React.useState('')
+  
+
   return (
     <div>
         <h4 className='text-lg  font-semibold mb-2'>Location</h4>
 
         <div>
-            <label className='flex sidebar-label-container'>
-                <span className='checkmark'></span>
-                <input type='radio' name="text" id="text" value="" onChange={handleChange}/>
-                <span className='sidebar-label'>All</span>
-            </label>
+            <InputField handleChange={handleChange} value="" title="All" name="text"/>
 
             <InputField handleChange={handleChange} value="london" title="London" name="text"/>
             <InputField handleChange={handleChange} value="seattle" title="Seattle" name="text"/>
