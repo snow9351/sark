7efa4f087c65b605ece3bee501b3290f1ec0da3b
souta9351 <56113417+snow9351@users.noreply.github.com>
2024-11-01 21:09:24 +0530

    Contact.jsx Working

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,6 @@
 import React, { useRef, useState } from "react";
 import { motion } from "framer-motion";
+import axios from "axios";
 // import emailjs from "@emailjs/browser";
 
 import { styles } from "../styles";
@@ -27,43 +28,34 @@ const Contact = () => {
     });
   };
 
-  const handleSubmit = (e) => {
-    // e.preventDefault();
-    // setLoading(true);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    try{
+          const res = await axios.post("http://localhost:5000/api/contact/submit", 
+               form, 
+               {
+                 headers: {
+                   'Authorization': import.meta.env.VITE_AUTH_MESSAGE
+                 }
+               });
+      console.log(res.data);
+      setLoading(false);
+      setForm({
+        name: "",
+        email: "",
+        message: "",
+      });
 
-    // emailjs
-    //   .send(
-    //     import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-    //     import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
-    //     {
-    //       from_name: form.name,
-    //       to_name: "JavaScript Mastery",
-    //       from_email: form.email,
-    //       to_email: "sujata@jsmastery.pro",
-    //       message: form.message,
-    //     },
-    //     import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
-    //   )
-    //   .then(
-    //     () => {
-    //       setLoading(false);
-    //       alert("Thank you. I will get back to you as soon as possible.");
 
-    //       setForm({
-    //         name: "",
-    //         email: "",
-    //         message: "",
-    //       });
-    //     },
-    //     (error) => {
-    //       setLoading(false);
-    //       console.error(error);
+    }
+    catch(err){
+      console.log(err);
+      setLoading(false);
+    }
+  }
 
-    //       alert("Ahh, something went wrong. Please try again.");
-    //     }
-    //   );
-    console.log("Form submitted");
-  };
+  
 
   return (
     <div
